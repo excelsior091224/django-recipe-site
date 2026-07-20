ce5ce86d9e2b460a42a9ed06d400a88ec4c29031
excelsior091224 <44745901+excelsior091224@users.noreply.github.com>
2026-07-20 08:50:55 +0000

    Add base template and recipe list view
    - Updated settings to include template and static file directories.
    - Created a base HTML template for consistent layout.
    - Added a recipe list view using Django's ListView.
    - Updated URLs to include the recipe list view as the index.
    - Created a recipe list template extending the base template.

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / Path("lib") / Path("templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / Path("assets")
+]
