5a5f081e37c1a3a9948a328aedc3f7922b377b02
excelsior091224 <44745901+excelsior091224@users.noreply.github.com>
2026-08-11 11:57:56 +0000

    20260811

--- staffroom/urls.py
+++ staffroom/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from .views import StaffroomTemplateView
 
 app_name = "staffroom"
 
 urlpatterns = [
+    path("", StaffroomTemplateView.as_view(), name="index"),
 ]
