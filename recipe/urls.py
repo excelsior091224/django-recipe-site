41551a59d294c2c909355076c616e34685b96dd7
excelsior091224 <44745901+excelsior091224@users.noreply.github.com>
2026-08-16 02:09:58 +0000

    レシピ作成、更新、削除のためのビューとフォームを追加し、関連するテンプレートを作成しました。また、スタッフルームのナビゲーションを更新しました。

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,19 +1,14 @@
 from django.urls import path
 
 from recipe.views import (
-    RecipeListView, RecipeCreateView,
-    RecipeDetailView, RecipeUpdateView, RecipeDeleteView
+    RecipeCreateView, RecipeDeleteView, RecipeUpdateView,  # === 編集 ===
+    RecipeListView, RecipeDetailView
 )
 
 app_name = "recipe"
 
 urlpatterns = [
     path('', RecipeListView.as_view(), name="index"),
-    path('create', RecipeCreateView.as_view(), name="create"),
-
-    path('<int:pk>/update', RecipeUpdateView.as_view(), name="update"),
-
-    path('<int:pk>/delete', RecipeDeleteView.as_view(), name="delete"),
 
     path('<int:pk>', RecipeDetailView.as_view(), name="detail"),
 ]
--- staffroom/views.py
+++ staffroom/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, DeleteView, UpdateView
+from django.urls import reverse, reverse_lazy
+from django.contrib import messages
 from recipe.models import Recipe
+from recipe.forms import RecipeForm  # === 追加 ===
+
 
 class StaffroomTemplateView(TemplateView):
     template_name = "staffroom/index.html"
@@ -9,8 +13,60 @@ class StaffroomTemplateView(TemplateView):
         context = super().get_context_data(**kwargs)
 
         if self.request.user.is_authenticated:
-            context['recipe_list'] = Recipe.objects.filter(user=self.request.user).prefetch_related("comment_set")
+            context['recipe_list'] = Recipe.objects.filter(
+                user=self.request.user).prefetch_related("comment_set")
         else:
             context['recipe_list'] = None
 
         return context
+
+
+class RecipeCreateView(CreateView):
+    model = Recipe
+    form_class = RecipeForm  # === 追加 ===
+    success_url = reverse_lazy("recipe:index")
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        data = request.POST.dict()
+        data['user'] = user.id
+        form = RecipeForm(data=data, files=request.FILES)
+
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        messages.success(self.request, "保存しました")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "保存できませんでした")
+        return super().form_invalid(form)
+
+
+class RecipeUpdateView(UpdateView):
+    model = Recipe
+    fields = ["title", "content", "description", "image"]
+
+    def get_success_url(self):
+        pk = self.kwargs.get("pk")
+        return reverse("recipe:detail", kwargs={"pk": pk})
+
+    def form_valid(self, form):
+        messages.success(self.request, "更新しました")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "更新できませんでした")
+        return super().form_invalid(form)
+
+
+class RecipeDeleteView(DeleteView):
+    model = Recipe
+    success_url = reverse_lazy("recipe:index")
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "削除しました")
+        return super().delete(request, *args, **kwargs)
